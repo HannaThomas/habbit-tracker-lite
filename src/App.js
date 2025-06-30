20f4173bca6d89f154699d9e56c2475700428765
HannaThomas <manikutty159@gmail.com>
2025-06-30 13:23:43 +0800

    Marking completion

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import DirectionsWalkIcon from '@mui/icons-material/DirectionsWalk';
 import PoolIcon from '@mui/icons-material/Pool';
 import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 import BookIcon from '@mui/icons-material/Book';
+import XIcon from '@mui/icons-material/X';
 import { useEffect, useState, useRef } from 'react';
-import { Box, Button, MenuItem, Select } from '@mui/material';
+import { Box, Button, MenuItem, Select, Checkbox } from '@mui/material';
 
 function App() {
   const [habit, setHabit] = useState('');
@@ -30,36 +31,47 @@ function App() {
   const deleteHabit = (id) => {
     setHabits(habits.filter((hab) => hab.id !== id));
   }
+  const onToggle = (id) => {
+    setHabits(
+      habits.map((hab) =>
+        (hab.id === id) ? { ...hab, checked: !hab.checked } : hab
+      ))
+  }
   //On Mounting
-  useEffect(()=>{
-    const savedHabits= JSON.parse(localStorage.getItem("habits"))
-    if(savedHabits) setHabits(savedHabits);
-  },[]);
+  useEffect(() => {
+    const savedHabits = JSON.parse(localStorage.getItem("habits"))
+    if (savedHabits) setHabits(savedHabits);
+  }, []);
   // On update
   useEffect(() => {
-    if(isFirstRender.current){
-      isFirstRender.current=false;
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
       return;
     }
     localStorage.setItem("habits", JSON.stringify(habits));
   }, [habits])
   return (
     <div>
-      <Select value={habitIcon} onChange={(e) => setHabitIcon(e.target.value)}>
+      Choose an icon:<Select value={habitIcon} onChange={(e) => setHabitIcon(e.target.value)}>
         <MenuItem value="Book"><BookIcon /></MenuItem>
         <MenuItem value="Walk"><DirectionsWalkIcon /></MenuItem>
         <MenuItem value="Swim"><PoolIcon /></MenuItem>
         <MenuItem value="Gaming"><SportsEsportsIcon /></MenuItem>
       </Select>
       <input placeholder='Add a habit' value={habit} onChange={(e) => setHabit(e.target.value)} />
-      <Button onClick={addHabit}>+</Button>
+      <Button variant="contained"
+        onClick={addHabit}>ADD</Button>
       <Box>
         <h1>Habit Tracker Lite</h1>
         {habits.map((hab) => (
-          <li key={hab.id}>
+          <div key={hab.id}>
+            <Checkbox
+              checked={hab.checked}
+              onChange={() => onToggle(hab.id)} />
             {hab.text}{iconMap[hab.icon]}
-            <Button onClick={() => { deleteHabit(hab.id) }}>-</Button>
-          </li>
+            <Button onClick={() => { deleteHabit(hab.id) }}><XIcon sx={{ color: 'red' }} /></Button>
+
+          </div>
         ))}
       </Box>
     </div>
